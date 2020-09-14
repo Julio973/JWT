9db4e5f4800f3b24c90b16e5d6c6436594673883
Julio973 <71272928+Julio973@users.noreply.github.com>
2020-09-14 10:04:09 -0500

    Add files via upload

---
+++ info.php
@@ -0,0 +1,13 @@
+<?php
+$usuario  = 'eduardo';
+$password = '123456';
+ if($usuario === if($usuario === 'eduardo' && $password === '123456'))
+    {
+        echo Auth::SignIn([
+            'id' => 1,
+            'name' => 'Eduardo'
+        ]);
+    });
+   
+
+?>
